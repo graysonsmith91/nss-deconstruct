9ac374d36f65f4244e1fc01a1ddfe3cd4b9a10d2
Grayson Smith <graysonsmith91@gmail.com>
2022-12-12 10:02:00 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const getKeys = () => {
+	console.log("Grab keys from bedroom")
+}
+
+const getDogBags = () => {
+	console.log("Grab a few dog bags")
+}
+
+const getLeash = () => {
+	console.log("Grab the dog leash")
+}
+
+const attachLeash = () => {
+	console.log("Grab keys from bedroom")
+}
+
+const openDoor = () => {
+	console.log("Open the front door with off hand")
+}
+
+const lockDoor = () => {
+	console.log("Lock door on the inside before shutting")
+}
+
+const walkDog = () => {
+	console.log("Walk dog around the block")
+}
+
+const throwAwayBags = () => {
+	console.log("Throw away any used dog bags")
+}
+
+const unlockBackDoor = () => {
+	console.log("Unlock back door with key")
+}
+
+const lockBackDoor = () => {
+	console.log("Lock back door with hand after inside")
+}
+
+getKeys()
+getDogBags()
+getLeash()
+attachLeash()
+openDoor()
+lockDoor()
+walkDog()
+throwAwayBags()
+unlockBackDoor()
+lockBackDoor()
